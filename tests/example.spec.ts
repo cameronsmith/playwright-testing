9f9153d13200297dea557a09b2ff592dfaebaebb
Cameron Smith <cameron.smith@lokalise.com>
2024-04-13 17:43:16 +0100

    Fill in text examples

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,47 @@
 import { test, expect } from '@playwright/test'
 
 test("Simple basic test", async ({page}) => {
-    await page.goto("https://www.example.com")
+    await page.goto('https://www.example.com')
     const pageTitle = await page.locator('h1')
     await expect(pageTitle).toContainText('Example Domain')
+})
+
+test("Clicking on element", async ({page}) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+    await page.click('text=Sign in')
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
+})
+
+test('Most common selectors', async ({page}) => {
+    test.skip()
+
+    // text
+    await page.click('text=some text')
+
+    // css selectors
+    await page.click('button')
+    await page.click('#id')
+    await page.click('.class')
+
+    // only visible css selctors
+    await page.click('.submit-button:visible')
+
+    // combinations
+    await page.click('#username .first')
+
+    // xpath
+    await page.click('//button')
+})
+
+test('Working with inputs', async ({page}) => {
+    await page.goto('http://zero.webappsecurity.com/index.html')
+    await page.click('#signin_button')
+    await page.fill('#user_login', 'some username')
+    await page.fill('#user_password', 'some password')
+    await page.click('text=Sign in')
+
+    const errorMessage = await page.locator('.alert-error')
+    await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
